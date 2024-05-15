bf16f942c211e6fdc108f22e093ef758f246cbbe
pmperrin <pierremichel.perrin@gmail.com>
2024-05-15 22:26:08 +0200

    move to folder

--- go-simple-i18n.go
+++
@@ -1,9 +0,0 @@
-package go-simple-i18n
-
-import (
-    "fmt"	
-)
-
-func GetText() {
-    fmt.Println("Hello, 世界")
-}
---
+++ simpleI18n/simpleI18n.go
@@ -0,0 +1,9 @@
+package simpleI18n
+
+import (
+	"fmt"
+)
+
+func GetText() {
+	fmt.Println("Hello, 世界")
+}
